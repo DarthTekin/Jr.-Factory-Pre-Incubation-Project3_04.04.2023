9443d7aa489b8b822043324ce99ce88b97134710
tekinteks@gmail.com <tekinteks@gmail.com>
2023-04-12 23:26:11 +0300

    StackController.cs added.

---
+++ Assets/Scripts/CupList.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StackController : MonoBehaviour
+{
+    public static StackController instance;
+
+    public List<GameObject> cups = new List<GameObject>();
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public float moveSpeed;
+    public float slideSpeed;
+
+    private Camera cam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+        if (Input.GetButton("Fire1"))
+        {
+            Move();
+        }
+    }
+
+    void Move()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = cam.transform.localPosition.z;
+
+        Ray ray = cam.ScreenPointToRay(mousePos);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            GameObject firstCup = StackController.instance.cups[0];
+            Vector3 hitVec = hit.point;
+            hitVec.y = firstCup.transform.localPosition.y;
+            hitVec.z = firstCup.transform.localPosition.z;
+
+            firstCup.transform.localPosition = Vector3.MoveTowards(firstCup.transform.localPosition, hitVec, Time.deltaTime* slideSpeed);
+        }
     }
 }
